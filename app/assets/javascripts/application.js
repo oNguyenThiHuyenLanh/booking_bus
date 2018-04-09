88d2feea44035ca2fca3727a1289922eb141fd26
grassme <lanh.ptit.tn@gmail.com>
2018-04-09 17:37:46 +0700

    validate-search-form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
+//= require jquery.validate
 //= require i18n
 //= require i18n.js
 //= require i18n/translations
