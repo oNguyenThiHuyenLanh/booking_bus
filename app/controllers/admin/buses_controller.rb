0177ed5afe99f60f47604629add988d3641a1a66
tuanAnh <tuananhnguyen170896@gmail.com>
2018-05-11 16:19:40 +0700

    create new bus UI

--- app/controllers/admin/buses_controller.rb
+++ app/controllers/admin/buses_controller.rb
@@ -1,7 +1,28 @@
 module Admin
   class BusesController < BaseController
+    before_action :filter, only: :new
+
     def index
       @buses = Bus.all
+    end
+
+    def new
+      @support = Supports::ManageBusesSupport.new
+
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    end
+
+    private
+
+    attr_reader :model_buses
+
+    def filter
+      seats = params[:no_seats]
+      floors = params[:no_floors]
+      @model_buses = ModelBus.filter seats: seats, floors: floors
     end
   end
 end
--- app/models/model_bus.rb
+++ app/models/model_bus.rb
@@ -2,10 +2,23 @@ class ModelBus < ApplicationRecord
   has_many :buses
   has_many :active_seat_coordinates
 
-  accepts_nested_attributes_for :active_seat_coordinates
-
   validates :amount_of_seats, :number_of_floors, :number_of_rows,
     :number_of_columns, presence: true
   validates :amount_of_seats, :number_of_floors, :number_of_rows,
     :number_of_columns, numericality: {greater_than: 0}
+
+  scope :filter_seats, ->(seats){where amount_of_seats: seats}
+  scope :filter_floors, ->(floors){where number_of_floors: floors}
+
+  accepts_nested_attributes_for :active_seat_coordinates
+
+  class << self
+    def filter params = {}
+      result = ModelBus.all
+      params.each do |key, value|
+        result = result.send("filter_#{key}", value) if value.present?
+      end
+      result
+    end
+  end
 end
