28519dfc5d24f85115a59441edcea99204701c39
tuanAnh <tuananhnguyen170896@gmail.com>
2018-06-26 15:30:44 +0700

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -265,11 +265,11 @@ ModelBus.create!(amount_of_seats: 40,
 ModelBus.create!(amount_of_seats: 40,
   number_of_floors: 1,
   number_of_rows: 10,
-  number_of_columns: 5)
+  number_of_columns: 6)
 ModelBus.create!(amount_of_seats: 50,
   number_of_floors: 2,
-  number_of_rows: 6,
-  number_of_columns: 5)
+  number_of_rows: 7,
+  number_of_columns: 6)
 
 ActiveSeatCoordinate.create!(column: 3,
   row: 1,
@@ -289,30 +289,98 @@ number = 2
 row = 1
 2.times do |n|
   row = row + 1
-    4.times do |m|
-      column = m+2
-      number = number + 1
-      ActiveSeatCoordinate.create!(column: column,
-        row: row,
-        number: number,
-        model_bus_id: 1,
-        type_of_seat_id: 2,
-        floor: 1)
-    end
+  4.times do |m|
+    column = m+2
+    number = number + 1
+    ActiveSeatCoordinate.create!(column: column,
+      row: row,
+      number: number,
+      model_bus_id: 1,
+      type_of_seat_id: 2,
+      floor: 1)
+  end
 end
 
 5.times do |n|
   row = row + 1
-    6.times do |m|
-      column = m+1
+  6.times do |m|
+    column = m+1
+    number = number + 1
+    ActiveSeatCoordinate.create!(column: column,
+      row: row,
+      number: number,
+      model_bus_id: 1,
+      type_of_seat_id: 1,
+      floor: 1)
+  end
+end
+
+number = 0
+(2..10).each do |row|
+  if row < 9
+    (1..6).each do |col|
+      if col!=3 && col!=4
+        number = number + 1
+        ActiveSeatCoordinate.create!(column: col,
+          row: row,
+          number: number,
+          model_bus_id: 2,
+          type_of_seat_id: 1,
+          floor: 1)
+      end
+    end
+  else
+    (1..6).each do |col|
       number = number + 1
-      ActiveSeatCoordinate.create!(column: column,
+      ActiveSeatCoordinate.create!(column: col,
         row: row,
         number: number,
-        model_bus_id: 1,
+        model_bus_id: 2,
         type_of_seat_id: 1,
         floor: 1)
     end
+  end
+end
+
+number = 0
+(2..7).each do |row|
+  (1..6).each do |col|
+    if col != 3 && col != 4
+      number = number + 1
+      ActiveSeatCoordinate.create!(column: col,
+        row: row,
+        number: number,
+        model_bus_id: 3,
+        type_of_seat_id: 1,
+        floor: 1)
+    end
+  end
+end
+
+(1..6).each do |row|
+  if row == 1
+    (1..6).each do |col|
+      number = number + 1
+      ActiveSeatCoordinate.create!(column: col,
+        row: row,
+        number: number,
+        model_bus_id: 3,
+        type_of_seat_id: 1,
+        floor: 2)
+    end
+  else
+    (1..6).each do |col|
+      if col!=3 && col!=4
+        number = number + 1
+        ActiveSeatCoordinate.create!(column: col,
+          row: row,
+          number: number,
+          model_bus_id: 3,
+          type_of_seat_id: 1,
+          floor: 2)
+      end
+    end
+  end
 end
 
 Bus.create!(name: "Xe khách Daewoo FX12 40 cho",
@@ -343,235 +411,354 @@ Bus.create!(name: "Hyundai Universe Noble 40 chỗ",
   number_of_seats: 40,
   model_bus_id: 1)
 
-Schedule.create!(price: 300000,
-  date: Date.parse("15/05/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 1,
-  route_id: 1,
-  interval_id: 1,
-  start_station_id: 1,
-  final_station_id: 6)
+  #1 Hanoi->NgheAn T1
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/01/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 1,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Schedule.create!(price: 300000,
-  date: Date.parse("15/05/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 1,
-  route_id: 2,
-  interval_id: 2,
-  start_station_id: 6,
-  final_station_id: 1)
+  #2 NgheAn->Hanoi T1
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/01/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 2,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Schedule.create!(price: 300000,
-  date: Date.parse("05/10/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 2,
-  route_id: 3,
-  interval_id: 1,
-  start_station_id: 1,
-  final_station_id: 6)
+  #3 Hanoi->NgheAn T2
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/02/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 1,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Schedule.create!(price: 300000,
-  date: Date.parse("05/10/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 2,
-  route_id: 4,
-  interval_id: 2,
-  start_station_id: 6,
-  final_station_id: 1)
+  #4 NgheAn->Hanoi T2
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/02/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 2,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Schedule.create!(price: 120000,
-  date: Date.parse("05/10/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 4,
-  route_id: 5,
-  interval_id: 1,
-  start_station_id: 1,
-  final_station_id: 9)
+  #5 Hanoi->NgheAn T3
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/03/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 1,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Schedule.create!(price: 120000,
-  date: Date.parse("05/10/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 4,
-  route_id: 6,
-  interval_id: 2,
-  start_station_id: 9,
-  final_station_id: 1)
+  #6 NgheAn->Hanoi T3
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/03/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 2,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("05/10/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 3,
-  route_id: 7,
-  interval_id: 1,
-  start_station_id: 2,
-  final_station_id: 11)
+  #7 Hanoi->NgheAn T4
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/04/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 1,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("05/10/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 3,
-  route_id: 8,
-  interval_id: 2,
-  start_station_id: 10,
-  final_station_id: 2)
+  #8 NgheAn->Hanoi T4
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/04/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 2,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("05/10/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 2,
-  route_id: 9,
-  interval_id: 1,
-  start_station_id: 1,
-  final_station_id: 12)
+  #9 Hanoi->NgheAn T5
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/05/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 1,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("06/10/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 2,
-  route_id: 10,
-  interval_id: 2,
-  start_station_id: 12,
-  final_station_id: 1)
+  #10 NgheAn->Hanoi T5
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/05/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 2,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Schedule.create!(price: 300000,
-  date: Date.parse("06/10/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 1,
-  route_id: 1,
-  interval_id: 1,
-  start_station_id: 1,
-  final_station_id: 6)
+  #11 Hanoi->NgheAn T6
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/06/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 1,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Schedule.create!(price: 300000,
-  date: Date.parse("06/10/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 1,
-  route_id: 2,
-  interval_id: 2,
-  start_station_id: 6,
-  final_station_id: 1)
+  #12 NgheAn->Hanoi T6
+  Schedule.create!(price: 150000,
+    date: Date.parse("27/06/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 2,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Schedule.create!(price: 120000,
-  date: Date.parse("06/10/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 4,
-  route_id: 5,
-  interval_id: 1,
-  start_station_id: 1,
-  final_station_id: 9)
+  #13 NgheAn->ThaiNguyen T6
+  Schedule.create!(price: 180000,
+    date: Date.parse("15/06/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 2,
+    route_id: 3,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Schedule.create!(price: 120000,
-  date: Date.parse("06/10/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 4,
-  route_id: 6,
-  interval_id: 2,
-  start_station_id: 1,
-  final_station_id: 9)
+  #14 ThaiNguyen->NgheAn T6
+  Schedule.create!(price: 180000,
+    date: Date.parse("15/06/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 2,
+    route_id: 4,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("06/10/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 3,
-  route_id: 7,
-  interval_id: 1,
-  start_station_id: 2,
-  final_station_id: 11)
+  #15 Hanoi->ThaiNguyen T6
+  Schedule.create!(price: 50000,
+    date: Date.parse("27/06/2018"),
+    time_start: "8:00",
+    time_spent: 120,
+    bus_id: 4,
+    route_id: 5,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 9)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("06/10/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 3,
-  route_id: 8,
-  interval_id: 2,
-  start_station_id: 10,
-  final_station_id: 2)
+  #16 ThaiNguyen->Hanoi T6
+  Schedule.create!(price: 50000,
+    date: Date.parse("27/06/2018"),
+    time_start: "13:30",
+    time_spent: 120,
+    bus_id: 4,
+    route_id: 6,
+    interval_id: 2,
+    start_station_id: 9,
+    final_station_id: 1)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("06/04/2018"),
-  time_start: "8:00",
-  time_spent: 300,
-  bus_id: 2,
-  route_id: 9,
-  interval_id: 1,
-  start_station_id: 1,
-  final_station_id: 12)
+  #17
+  Schedule.create!(price: 60000,
+    date: Date.parse("28/06/2018"),
+    time_start: "8:00",
+    time_spent: 120,
+    bus_id: 3,
+    route_id: 7,
+    interval_id: 1,
+    start_station_id: 2,
+    final_station_id: 11)
 
-Schedule.create!(price: 160000,
-  date: Date.parse("06/04/2018"),
-  time_start: "13:30",
-  time_spent: 300,
-  bus_id: 2,
-  route_id: 10,
-  interval_id: 2,
-  start_station_id: 12,
-  final_station_id: 1)
+  Schedule.create!(price: 60000,
+    date: Date.parse("28/06/2018"),
+    time_start: "13:30",
+    time_spent: 120,
+    bus_id: 3,
+    route_id: 8,
+    interval_id: 2,
+    start_station_id: 10,
+    final_station_id: 2)
 
-Bill.create!(total_price: 780000,
-  total_ticket: 2,
-  user_id: 2,
-  schedule_id: 1
-  )
-  BookedSeat.create!(no_of_seat: 1,
-    bill_id: 1)
-  BookedSeat.create!(no_of_seat: 5,
-    bill_id: 1)
+  Schedule.create!(price: 160000,
+    date: Date.parse("29/06/2018"),
+    time_start: "8:00",
+    time_spent: 240,
+    bus_id: 2,
+    route_id: 9,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 12)
 
-Bill.create!(total_price: 360000,
-  total_ticket: 1,
-  user_id: 2,
-  schedule_id: 1)
-  BookedSeat.create!(no_of_seat: 21,
-    bill_id: 2)
+  Schedule.create!(price: 160000,
+    date: Date.parse("29/06/2018"),
+    time_start: "13:30",
+    time_spent: 240,
+    bus_id: 2,
+    route_id: 10,
+    interval_id: 2,
+    start_station_id: 12,
+    final_station_id: 1)
 
-Bill.create!(total_price: 750000,
-  total_ticket: 2,
-  user_id: 4,
-  schedule_id: 1)
-  BookedSeat.create!(no_of_seat: 22,
-    bill_id: 3)
-  BookedSeat.create!(no_of_seat: 6,
-    bill_id: 3)
+  # Hanoi -> Nghe An
+  Schedule.create!(price: 150000,
+    date: Date.parse("29/06/2018"),
+    time_start: "8:00",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 1,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 6)
 
-Bill.create!(total_price: 90000,
-  total_ticket: 1,
-  user_id: 4,
-  schedule_id: 2)
-  BookedSeat.create!(no_of_seat: 6,
-    bill_id: 4)
+  # Nghe An -> Hanoi
+  Schedule.create!(price: 150000,
+    date: Date.parse("29/06/2018"),
+    time_start: "13:30",
+    time_spent: 300,
+    bus_id: 1,
+    route_id: 2,
+    interval_id: 2,
+    start_station_id: 6,
+    final_station_id: 1)
 
-Bill.create!(total_price: 180000,
-  total_ticket: 2,
-  user_id: 4,
-  schedule_id: 2)
-  BookedSeat.create!(no_of_seat: 7,
-    bill_id: 5)
-  BookedSeat.create!(no_of_seat: 10,
-    bill_id: 5)
+  Schedule.create!(price: 50000,
+    date: Date.parse("29/06/2018"),
+    time_start: "8:00",
+    time_spent: 120,
+    bus_id: 4,
+    route_id: 5,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 9)
 
-Bill.create!(total_price: 180000,
-  total_ticket: 3,
-  user_id: 4,
-  schedule_id: 2)
-  BookedSeat.create!(no_of_seat: 11,
-    bill_id: 6)
-  BookedSeat.create!(no_of_seat: 12,
-    bill_id: 6)
-  BookedSeat.create!(no_of_seat: 19,
-    bill_id: 6)
+  Schedule.create!(price: 50000,
+    date: Date.parse("29/06/2018"),
+    time_start: "13:30",
+    time_spent: 120,
+    bus_id: 4,
+    route_id: 6,
+    interval_id: 2,
+    start_station_id: 1,
+    final_station_id: 9)
+
+  Schedule.create!(price: 60000,
+    date: Date.parse("29/06/2018"),
+    time_start: "8:00",
+    time_spent: 120,
+    bus_id: 3,
+    route_id: 7,
+    interval_id: 1,
+    start_station_id: 2,
+    final_station_id: 11)
+
+  Schedule.create!(price: 60000,
+    date: Date.parse("29/06/2018"),
+    time_start: "13:30",
+    time_spent: 120,
+    bus_id: 3,
+    route_id: 8,
+    interval_id: 2,
+    start_station_id: 10,
+    final_station_id: 2)
+
+  Schedule.create!(price: 160000,
+    date: Date.parse("01/07/2018"),
+    time_start: "8:00",
+    time_spent: 240,
+    bus_id: 2,
+    route_id: 9,
+    interval_id: 1,
+    start_station_id: 1,
+    final_station_id: 12)
+
+  Schedule.create!(price: 160000,
+    date: Date.parse("01/07/2018"),
+    time_start: "13:30",
+    time_spent: 240,
+    bus_id: 2,
+    route_id: 10,
+    interval_id: 2,
+    start_station_id: 12,
+    final_station_id: 1)
+
+  Bill.create!(total_price: 300000,
+    total_ticket: 2,
+    user_id: 2,
+    schedule_id: 1
+    )
+    BookedSeat.create!(no_of_seat: 1,
+      bill_id: 1)
+    BookedSeat.create!(no_of_seat: 5,
+      bill_id: 1)
+
+  Bill.create!(total_price: 150000,
+    total_ticket: 1,
+    user_id: 2,
+    schedule_id: 1)
+    BookedSeat.create!(no_of_seat: 21,
+      bill_id: 2)
+
+  Bill.create!(total_price: 300000,
+    total_ticket: 2,
+    user_id: 4,
+    schedule_id: 1)
+    BookedSeat.create!(no_of_seat: 22,
+      bill_id: 3)
+    BookedSeat.create!(no_of_seat: 6,
+      bill_id: 3)
+
+  Bill.create!(total_price: 150000,
+    total_ticket: 1,
+    user_id: 4,
+    schedule_id: 2)
+    BookedSeat.create!(no_of_seat: 6,
+      bill_id: 4)
+
+  Bill.create!(total_price: 300000,
+    total_ticket: 2,
+    user_id: 4,
+    schedule_id: 2)
+    BookedSeat.create!(no_of_seat: 7,
+      bill_id: 5)
+    BookedSeat.create!(no_of_seat: 10,
+      bill_id: 5)
+
+  Bill.create!(total_price: 450000,
+    total_ticket: 3,
+    user_id: 4,
+    schedule_id: 2)
+    BookedSeat.create!(no_of_seat: 11,
+      bill_id: 6)
+    BookedSeat.create!(no_of_seat: 12,
+      bill_id: 6)
+    BookedSeat.create!(no_of_seat: 19,
+      bill_id: 6)
